8d7e26d4a3d0c4eec5d9564807602db48a1f3913
rajan <rajanrrajan2003@gmail.com>
2024-01-01 18:49:18 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'resume';
+export class AppComponent  {
+
+  // browserInfo: any = {};
+
+  // ngOnInit(): void {
+  //   this.detectBrowserInfo();
+  // }
+
+  // detectBrowserInfo(): void {
+  //   this.browserInfo.userAgent = navigator.userAgent;
+  //   this.browserInfo.appName = navigator.appName;
+  //   this.browserInfo.netMark = navigator.vendor;
+  //   this.browserInfo.appVersion = navigator.appVersion;
+  //   this.browserInfo.platform = navigator.platform;
+  //   this.browserInfo.language = navigator.language;
+  //   this.browserInfo.onLine = navigator.onLine;
+  //   this.browserInfo.cookieEnabled = navigator.cookieEnabled;
+  //   this.browserInfo.javaEnabled = navigator.javaEnabled();
+  //   this.browserInfo.javaScriptEnabled = true; // JavaScript is always enabled in modern browsers
+  //   this.browserInfo.colorDepth = screen.colorDepth;
+  //   this.browserInfo.screenHeight = screen.height;
+  //   this.browserInfo.screenWidth = screen.width;
+  //   this.browserInfo.timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+  // }
+
+
+
+  // browserInfo: any = {};
+
+  // ngOnInit(): void {
+  //   this.detectBrowserInfo();
+  // }
+
+  // detectBrowserInfo(): void {
+  //   this.browserInfo.cookieEnabled = navigator.cookieEnabled;
+  //   this.browserInfo.javaScriptEnabled = navigator.javaEnabled();
+  // }
 }
